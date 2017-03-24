f63dbdbbb94d15197419852f9e39ecd43cb10f0a
poniente <poniente@ponienteDAM>
2017-03-24 11:39:02 +0100

    Tercera subida del proyecto: *Fallos: Sin fallos encontrados. *Enunciado de la tercera parte del proyecto Desarrollar una clase genérica VehiculoCarga. En esta clase es un tipo de vehículo dedicado al transporte de mercancías que contendrá:
    •    La carga máxima del vehículo (Tara) en toneladas.
    •    Los constructores por defecto y con parámetros del vehículo
    •    Los métodos get y set de todos los atributos.
    •	Un método abstracto alquiler que no  reciba  nada y devuelva  un double con el importe del alquiler. Este dependerá de si finalmente es una furgoneta o un camión
    •	Un  método  recibo  que  no  se  pueda  modificar  que  muestre  por pantalla toda la información del alquiler del vehículo:
    
    Matricula: valor
    Duración: valor
    Tara: valor Toneladas
    Importe: valor €

---
+++ src/ejercicio_3_t_4_ed/VehiculoCarga.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_3_t_4_ed;
+
+/**
+ *
+ * @author poniente
+ */
+public abstract class  VehiculoCarga extends Vehiculo
+{
+
+    /**
+     * Variable de tipo double
+     */
+    double tara;
+
+    /**
+     * Constructor por defecto
+     */
+    public VehiculoCarga() 
+    {
+        tara = 0.0;
+    }
+
+    /**
+     * Constructor con parametros
+     *
+     * @param tara del vehiculo
+     * @param matricula del vehiculo
+     * @param dias alquilados
+     */
+    public VehiculoCarga(double tara, String matricula, int dias) 
+    {
+        super(matricula, dias);
+        this.tara = tara;
+    }
+
+    /**
+     * Metodos get y set
+     *
+     * @return
+     */
+    public double getTara() 
+    {
+        return tara;
+    }
+
+    public void setTara(double tara) 
+    {
+        this.tara = tara;
+    }
+
+    /**
+     * Metodo abstracto
+     *
+     * @return
+     */
+    @Override
+    public abstract double importeAlquiler();
+    /**
+     * Metodo recibo que nos devuelve los valores como en la clase anterior 
+     * Pero en esta quitemoa el numero_plazas_vehiculo y ponemos tara
+     */
+
+    public void recibo() 
+    {
+        System.out.println("Matricula: " + this.getMatricula());
+        System.out.println("Duración: " + this.getDias());
+        System.out.println("Tara: " + this.getTara());
+        System.out.println("Importe: " + this.importeAlquiler());
+    }
+
+}
