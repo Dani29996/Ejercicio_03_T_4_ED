aeabdef904b428eca6b50d8d1fc3a03ffa003b1f
poniente <poniente@ponienteDAM>
2017-03-24 09:37:40 +0100

    Primera subida del proyecto. Tenemos la primera clase que nos pide el proyecto. Aqui dejo el primer enunciado:
    Desarrollar  una  clase  genérica  Vehiculo.  En  esta  clase  aparecerán  los atributos y métodos comunes a todos los vehículos:
    
    •    La matrícula del vehículo y la duración del alquiler en días.
    •    Los constructores por defecto y con parámetros del vehículo.
    •    Los métodos get y set de todos los atributos.
    •	Un método abstracto importeAlquiler que no reciba nada y devuelva un double con el importe del alquiler. Este dependerá del tipo de vehículo que finalmente sea.
    •	Un  método  abstracto  recibo  que  muestre  por  pantalla  toda  la información del alquiler del vehículo, el formato del recibo depende de si es un vehículo de transporte o de carga.

---
+++ src/ejercicio_3_t_4_ed/Vehiculo.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_3_t_4_ed;
+
+/**
+ *
+ * @author poniente
+ */
+public abstract class Vehiculo 
+{
+    /**
+     *  Variables de tipo String e Int
+     */
+    String matricula;
+    int dias;
+    
+
+    /**
+     * Constructor por defecto
+     */
+    public Vehiculo() 
+    {
+      matricula = "";
+      dias = 0;
+      
+    }
+
+    /**
+     * Constructor con parametros 
+     * @param matricula
+     * @param dias 
+     */
+    public Vehiculo(String matricula, int dias) 
+    {
+        this.matricula = matricula;
+        this.dias = dias;
+        
+    }
+
+      /**
+     * Metodos get y set
+     * @return 
+     */
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) 
+    {
+        this.matricula = matricula;
+    }
+
+    public int getDias() 
+    {
+        return dias;
+    }
+
+    public void setDias(int dias) 
+    {
+        this.dias = dias;
+    }
+    /**
+     * Metodo abstracto que devolvera un double
+     * @return 
+     */
+     public abstract  double importeAlquiler();
+     /**
+      * Metodo abstracto que devolvera un double 
+      * @return 
+      */
+     
+     public abstract double recibo();
+     
+     
+     
+    
+        
+    
+    
+    
+    
+    
+    
+}
